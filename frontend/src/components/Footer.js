7205ec2a6513b39b7dbfaa78723cff73a404bec9
Timeness <careless@flash.co>
2025-05-05 15:01:11 +0530

    done

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="py-6 bg-gray-900 text-white text-center">
+      <p className="font-montserrat">
+        &copy; 2025 Soumo. Built with ❤️ and Next.js.
+      </p>
+    </footer>
+  );
+}
