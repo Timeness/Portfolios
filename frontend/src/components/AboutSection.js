90ceda91f4f5dac58a3c1f2513f2c33b4052071b
Timeness <careless@flash.co>
2025-05-05 15:47:18 +0530

    done

--- frontend/src/components/AboutSection.js
+++ frontend/src/components/AboutSection.js
@@ -1,5 +1,5 @@
 'use client';
-import { motion } from 'framer';
+import { motion } from 'framer-motion';
 import { Card, CardBody } from '@nextui-org/react';
 
 export default function AboutSection() {
