f606a773bbb7b25def14286b667f8731b51bd8ea
Timeness <careless@flash.co>
2025-05-05 15:11:10 +0530

    done

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'soumo-blue': '#1E3A8A',
+        'soumo-purple': '#A586FF',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  darkMode: 'class',
+  plugins: [require('@nextui-org/theme')],
+};
